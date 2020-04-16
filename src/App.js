f0c2d95448a61da2fb76d110cdd715c29e760b32
Dmitrii Nazarenko <deemaneken@gmail.com>
2020-04-16 14:21:15 +0200

    new style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MovieSearch from './MovieSearch';
+import './index.css';
 
 
 function App() {
--- src/MovieSearch.js
+++ src/MovieSearch.js
@@ -1,40 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
+import Search from './Search';
+import { Spinner, Alert } from 'reactstrap';
 
 const MovieSearch = () => {
-
-	const [title, setTitle] = useState("limitless");
 	
-	useEffect(() => {
-		async function filmData() {
-			try {
-				const response = await axios.get(`http://www.omdbapi.com/?t=${title}&apikey=f0076aec`);
+	const [movieData, setData] = useState({});
+	const [name, setName] = useState(null);
+	const [fadeIn, setFadeIn] = useState(false);
+	//const [preview, setPreview] = useState({});
 
-				console.log(response);
-			} catch (error) {
-				console.log(error);
-			}
+	const toggle = () => setFadeIn(true);
+	const fade = () => setFadeIn(!true);
+	const newTitle = e => setName(e.target.value);
+
+	async function filmData() {
+		try {
+			const response = await axios.get(`http://www.omdbapi.com/?t=${name}&apikey=f0076aec`);				
+			setData(response.data);
+		} catch (error) {
+			return(
+				<Alert color="danger">
+					<h4>Error!</h4>
+					<p>{error}</p>
+				</Alert>
+			);
 		}
+	}
 
-		filmData();
-	}, [title]);
+
+	if(!movieData){
+		return (<Spinner color="danger"></Spinner>);
+	}
 
 	return(
 		<div className="container">
-			<div className="search-bar">
-				<input type="text"/>
-			</div>
-			<div className="movie-card">
-				<img src="#" alt="film-image"/>
-				<div className="card-info">
-					<h1 className="title">Film name</h1>
-					<div className="released">Date</div>
-					<div className="rating">Rating</div>
-					<div className="runtime">Duration</div>
-					<div className="about">Some info about the film</div>
-				</div>
-			</div>
-		</div>
+			<Search onSubmit={e => {
+						e.preventDefault(); 
+						filmData();
+						toggle();
+					}}
+					value={name}
+					onChange={e => {newTitle(e); fade();}}
+			/>
+			<MovieCard data={movieData} fadeIn={fadeIn} />
+	    </div>
 	);
 }
 
